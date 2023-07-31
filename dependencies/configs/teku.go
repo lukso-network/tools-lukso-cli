71fcef2535ecb33bed6e823312532ca6d87dcd2f
Wolmin <lamonos123@gmail.com>
2023-07-31 12:14:12 +0200

    Add teku beacon config files to init

--- dependencies/configs/teku.go
+++ dependencies/configs/teku.go
@@ -12,5 +12,15 @@ var (
 			name:     tekuTestnetChainConfigDependencyName,
 			filePath: TestnetConfig + "/" + TekuChainConfigYamlPath,
 		},
+		tekuMainnetConfigDependencyName: &clientConfig{
+			url:      "https://raw.githubusercontent.com/lukso-network/network-configs/main/mainnet/teku/teku.yaml",
+			name:     tekuMainnetChainConfigDependencyName,
+			filePath: MainnetConfig + "/" + TekuYamlPath,
+		},
+		tekuTestnetConfigDependencyName: &clientConfig{
+			url:      "https://raw.githubusercontent.com/lukso-network/network-configs/main/testnet/teku/teku.yaml",
+			name:     tekuTestnetChainConfigDependencyName,
+			filePath: TestnetConfig + "/" + TekuYamlPath,
+		},
 	}
 )
