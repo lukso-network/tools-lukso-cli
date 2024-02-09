263be8de4e66d4f1bcc60d9b1588cb87678fae82
Wolmin <lamonos123@gmail.com>
2024-02-09 14:23:20 +0100

    feat: update geth

--- common/shared.go
+++ common/shared.go
@@ -4,10 +4,10 @@ const (
 	ConfigPerms   = 0750
 	SlotsPerEpoch = 32
 
-	GethTag        = "1.13.5"
+	GethTag        = "1.13.12"
 	ErigonTag      = "2.55.0"
 	PrysmTag       = "v4.0.8"
 	LighthouseTag  = "v4.5.0"
 	TekuTag        = "23.11.0"
-	GethCommitHash = "916d6a44"
+	GethCommitHash = "02eb36af"
 )
