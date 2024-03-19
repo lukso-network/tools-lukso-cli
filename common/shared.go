d99602b4d4f013c2c631b9b43ac3af6dcf2b00bc
mxmar <macht4tech@gmail.com>
2024-03-19 10:12:54 +0100

    Apply Dencun EL and CL clients
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- common/shared.go
+++ common/shared.go
@@ -4,10 +4,10 @@ const (
 	ConfigPerms   = 0750
 	SlotsPerEpoch = 32
 
-	GethTag        = "1.13.12"
-	ErigonTag      = "2.57.3"
-	PrysmTag       = "v4.2.1"
-	LighthouseTag  = "v4.6.0"
-	TekuTag        = "24.1.1"
-	GethCommitHash = "02eb36af"
+	GethTag        = "1.13.14"
+	ErigonTag      = "2.58.2"
+	PrysmTag       = "v5.0.1"
+	LighthouseTag  = "v5.1.1"
+	TekuTag        = "24.3.0"
+	GethCommitHash = "2bd6bd0"
 )
