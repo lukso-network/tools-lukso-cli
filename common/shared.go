a55c9b3d473f6e9211509cdfeb828e8dfa9ab311
mxmar <macht4tech@gmail.com>
2024-03-21 11:31:03 +0100

    Bump Erigon version
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- common/shared.go
+++ common/shared.go
@@ -5,7 +5,7 @@ const (
 	SlotsPerEpoch = 32
 
 	GethTag        = "1.13.14"
-	ErigonTag      = "2.58.2"
+	ErigonTag      = "2.59.0"
 	PrysmTag       = "v5.0.1"
 	LighthouseTag  = "v5.1.1"
 	TekuTag        = "24.3.0"
