2f0395d0282edb9bbd6b869bee6d38a25062b4aa
Wolmin <lamonos123@gmail.com>
2024-04-22 11:50:35 +0200

    Bump versions

--- common/shared.go
+++ common/shared.go
@@ -4,10 +4,10 @@ const (
 	ConfigPerms   = 0750
 	SlotsPerEpoch = 32
 
-	GethTag        = "1.13.14"
+	GethTag        = "1.13.15"
 	ErigonTag      = "2.59.3"
-	PrysmTag       = "v5.0.2"
+	PrysmTag       = "v5.0.3"
 	LighthouseTag  = "v5.1.3"
 	TekuTag        = "24.3.1"
-	GethCommitHash = "2bd6bd01"
+	GethCommitHash = "c5ba367e"
 )
