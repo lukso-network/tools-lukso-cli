ffc2ce9c18f8213368ae297b11e2d902ec30f9e2
Wolmin <lamonos123@gmail.com>
2024-05-06 11:40:07 +0200

    Upgrade clients

--- common/shared.go
+++ common/shared.go
@@ -4,10 +4,10 @@ const (
 	ConfigPerms   = 0750
 	SlotsPerEpoch = 32
 
-	GethTag        = "1.13.15"
+	GethTag        = "1.14.0"
 	ErigonTag      = "2.59.3"
 	PrysmTag       = "v5.0.3"
 	LighthouseTag  = "v5.1.3"
-	TekuTag        = "24.3.1"
+	TekuTag        = "24.4.0"
 	GethCommitHash = "c5ba367e"
 )
