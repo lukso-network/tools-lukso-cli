d569ba3e74c96a3941c4f7eec046b97bf2a3358f
Matt <macht4tech@gmail.com>
2024-05-06 22:14:46 +0700

    Upgrade EL and CL clients (#235)
    * Apply Dencun EL and CL clients
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Apply Dencun EL and CL clients
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Bump Erigon version
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Bump Lighthouse version
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Change client versiopns in README
    
    * Fix teku version
    
    * Update EL and CL clients
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    
    * Bump versions
    
    * Upgrade clients
    
    * Fix incorrect commit hash
    
    ---------
    
    Signed-off-by: mxmar <macht4tech@gmail.com>
    Co-authored-by: Wolmin <lamonos123@gmail.com>

--- common/shared.go
+++ common/shared.go
@@ -4,10 +4,10 @@ const (
 	ConfigPerms   = 0750
 	SlotsPerEpoch = 32
 
-	GethTag        = "1.13.12"
-	ErigonTag      = "2.57.3"
-	PrysmTag       = "v4.2.1"
-	LighthouseTag  = "v4.6.0"
-	TekuTag        = "24.1.1"
-	GethCommitHash = "02eb36af"
+	GethTag        = "1.14.0"
+	ErigonTag      = "2.59.3"
+	PrysmTag       = "v5.0.3"
+	LighthouseTag  = "v5.1.3"
+	TekuTag        = "24.4.0"
+	GethCommitHash = "87246f3c"
 )
