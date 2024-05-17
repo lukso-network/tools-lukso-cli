646639c6fd0c85a16c980ad3889f16788283bdfa
Mateusz Marciniak <macht4tech@gmail.com>
2024-05-17 17:45:46 +0200

    Change EL&CL clients versions
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- common/shared.go
+++ common/shared.go
@@ -4,9 +4,9 @@ const (
 	ConfigPerms   = 0750
 	SlotsPerEpoch = 32
 
-	GethTag        = "1.14.0"
+	GethTag        = "1.13.15"
 	ErigonTag      = "2.59.3"
-	PrysmTag       = "v5.0.3"
+	PrysmTag       = "v4.2.1"
 	LighthouseTag  = "v5.1.3"
 	TekuTag        = "24.4.0"
 	GethCommitHash = "87246f3c"
