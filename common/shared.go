26dbfd578082c79c47ecd817be27c3a0b2093de2
Wolmin <lamonos123@gmail.com>
2024-06-06 10:02:15 +0200

    fix: new Geth version

--- common/shared.go
+++ common/shared.go
@@ -4,10 +4,10 @@ const (
 	ConfigPerms   = 0750
 	SlotsPerEpoch = 32
 
-	GethTag        = "1.13.15"
+	GethTag        = "1.14.4"
 	ErigonTag      = "2.59.3"
 	PrysmTag       = "v4.2.1"
 	LighthouseTag  = "v5.1.3"
 	TekuTag        = "24.4.0"
-	GethCommitHash = "c5ba367e"
+	GethCommitHash = "5550d839"
 )
