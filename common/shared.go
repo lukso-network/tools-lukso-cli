321dbcf39aff8fd1258ea66e26accdf071885913
Wolmin <lamonos123@gmail.com>
2024-06-13 15:56:14 +0200

    Upgrade geth further + update lighthouse

--- common/shared.go
+++ common/shared.go
@@ -4,10 +4,10 @@ const (
 	ConfigPerms   = 0750
 	SlotsPerEpoch = 32
 
-	GethTag        = "1.14.4"
+	GethTag        = "1.14.5"
 	ErigonTag      = "2.59.3"
 	PrysmTag       = "v4.2.1"
-	LighthouseTag  = "v5.1.3"
+	LighthouseTag  = "v5.2.0"
 	TekuTag        = "24.4.0"
-	GethCommitHash = "5550d839"
+	GethCommitHash = "0dd173a7"
 )
