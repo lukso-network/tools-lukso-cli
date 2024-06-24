1ec0b9715cf81aef62497696dbcb4c5ce0577805
Wolmin <lamonos123@gmail.com>
2024-06-24 13:38:10 +0200

    Change nethermind configs url to actual nethermind

--- dependencies/configs/nethermind.go
+++ dependencies/configs/nethermind.go
@@ -2,12 +2,12 @@ package configs
 
 var NethermindConfigDependencies = map[string]ClientConfigDependency{
 	nethermindMainnetConfigName: &clientConfig{
-		url:      "https://raw.githubusercontent.com/lukso-network/network-configs/main/mainnet/geth/geth.toml",
+		url:      "https://raw.githubusercontent.com/lukso-network/network-configs/main/mainnet/nethermind/nethermind.cfg",
 		name:     nethermindMainnetConfigName,
 		filePath: MainnetConfig + "/" + NethermindCfgPath,
 	},
 	nethermindTestnetConfigName: &clientConfig{
-		url:      "https://raw.githubusercontent.com/lukso-network/network-configs/main/testnet/geth/geth.toml",
+		url:      "https://raw.githubusercontent.com/lukso-network/network-configs/main/testnet/nethermind/nethermind.cfg",
 		name:     nethermindTestnetConfigName,
 		filePath: TestnetConfig + "/" + NethermindCfgPath,
 	},
