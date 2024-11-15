54572301a2dabde9feee95757b3b652e19602eb7
Wolmin <44748271+Wolmin@users.noreply.github.com>
2024-11-15 11:49:53 +0100

    Bump nimbus2 tag

--- common/shared.go
+++ common/shared.go
@@ -11,10 +11,10 @@ const (
 	LighthouseTag        = "v5.2.1"
 	TekuTag              = "24.6.1"
 	BesuTag              = "24.7.0"
-	Nimbus2Tag           = "24.9.0"
+	Nimbus2Tag           = "24.10.0"
 	GethCommitHash       = "aa55f5ea"
 	NethermindCommitHash = "220b5b85"
-	Nimbus2CommitHash    = "f54a0366"
+	Nimbus2CommitHash    = "c4037d63"
 
 	ClientDepsFolder = "clients" // folder in which client dependencies are stored
 )
