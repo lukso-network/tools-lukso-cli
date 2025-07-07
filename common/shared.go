f2e875d317c2af92c4a2c7e4775e898535cbf8cf
Wolmin <lamonos123@gmail.com>
2025-07-07 15:41:58 +0200

    Update clients

--- common/shared.go
+++ common/shared.go
@@ -4,16 +4,16 @@ const (
 	ConfigPerms   = 0o750
 	SlotsPerEpoch = 32
 
-	GethTag              = "1.15.11"
-	ErigonTag            = "v3.0.3"
-	NethermindTag        = "1.31.10"
-	BesuTag              = "25.4.1"
-	PrysmTag             = "v6.0.2"
+	GethTag              = "1.16.1"
+	ErigonTag            = "v3.0.12"
+	NethermindTag        = "1.32.2"
+	BesuTag              = "25.7.0"
+	PrysmTag             = "v6.0.4"
 	LighthouseTag        = "v7.0.1"
-	TekuTag              = "25.4.1"
+	TekuTag              = "25.6.0"
 	Nimbus2Tag           = "25.5.0"
-	GethCommitHash       = "36b2371c"
-	NethermindCommitHash = "f62cfede"
+	GethCommitHash       = "12b4131f"
+	NethermindCommitHash = "d3e7eb98"
 	Nimbus2CommitHash    = "d2f23389"
 
 	ClientDepsFolder = "clients" // folder in which client dependencies are stored
