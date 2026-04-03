f57204402efe206949a8dfa3e6a4ae4efed144bc
Wolmin <lamonos123@gmail.com>
2026-04-03 10:12:22 +0200

    Bump geth to 1.17.2

--- common/shared.go
+++ common/shared.go
@@ -4,7 +4,7 @@ const (
 	ConfigPerms   = 0o750
 	SlotsPerEpoch = 32
 
-	GethTag              = "1.17.0"
+	GethTag              = "1.17.2"
 	ErigonTag            = "v3.3.8"
 	NethermindTag        = "1.36.0"
 	BesuTag              = "26.1.0"
@@ -12,7 +12,7 @@ const (
 	LighthouseTag        = "v8.1.1"
 	TekuTag              = "26.2.0"
 	Nimbus2Tag           = "26.3.0"
-	GethCommitHash       = "0cf3d3ba"
+	GethCommitHash       = "be4dc0c4"
 	NethermindCommitHash = "31cb81b7"
 	Nimbus2CommitHash    = "6aa946f8"
 
